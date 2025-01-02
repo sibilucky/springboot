57dc37d4ea963210c264d99ab4d9ea9b41a66c17
sibilucky <selvimuthusamy785@gmail.com>
2025-01-02 22:37:10 +0530

    Your commit message

---
+++ MyApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
+
